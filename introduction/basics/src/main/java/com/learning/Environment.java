4b7d619fa9f15a0a387dd918a20ea29cdf146238
AngelKode <200angel@live.com.mx>
2023-08-07 15:48:41 -0600

    System environment variables learned

---
+++ introduction/basics/src/main/java/com/learning/Environment.java
@@ -0,0 +1,15 @@
+package com.learning;
+
+import java.util.Map;
+
+public class Environment {
+    public static void main(String[] args) {
+        
+        Map<String,String> myEnv = System.getenv();
+        
+        System.out.println("Environment values: ");
+        for(String key : myEnv.keySet()){
+            System.out.println(key + " => " + myEnv.get(key)) ;
+        }
+    }
+}
