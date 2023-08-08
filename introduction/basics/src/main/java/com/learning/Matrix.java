4179bf72229597f53c6d331000a5025696cd4fd3
AngelKode <200angel@live.com.mx>
2023-08-08 14:28:44 -0600

    Matrix relearned

---
+++ introduction/basics/src/main/java/com/learning/Matrix.java
@@ -0,0 +1,27 @@
+package com.learning;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class Matrix {
+    public static void main(String[] args) {
+        
+        int[][] myMatrix = new int[2][4];
+        System.out.println(myMatrix.length);
+
+        int[][] variableColumns = new int[2][];
+
+        variableColumns[0] = new int[12];
+        variableColumns[1] = new int[6];
+
+        Arrays.fill(variableColumns[0],1);
+        Arrays.fill(variableColumns[1],2);
+
+        for(int arr[] : variableColumns){
+            for(int val : arr){
+                System.out.print(val + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
