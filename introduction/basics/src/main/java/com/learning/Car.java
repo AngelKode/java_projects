4ff94a65cf82d52ce5671edc3ca37f7a879402b2
AngelKode <200angel@live.com.mx>
2023-08-08 15:58:26 -0600

    Implement lombok library

---
+++ introduction/basics/src/main/java/com/learning/Car.java
@@ -0,0 +1,11 @@
+package com.learning;
+
+import lombok.Data;
+
+/**
+* @param modelName the model of the car 
+*/
+public @Data class Car{
+    private String modelName;
+    private int yearModel;
+}
---
+++ introduction/basics/src/main/java/com/learning/Classes.java
@@ -0,0 +1,8 @@
+package com.learning;
+
+public class Classes {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        System.out.println(myCar.toString());
+    }
+}
