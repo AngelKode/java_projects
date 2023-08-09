6627624601be9469a51eaea43220ab03f2d7705b
AngelKode <200angel@live.com.mx>
2023-08-09 11:14:31 -0600

    Add switch exercise

--- introduction/basics/src/main/java/com/learning/Classes.java
+++ introduction/basics/src/main/java/com/learning/Classes.java
@@ -4,5 +4,14 @@ public class Classes {
     public static void main(String[] args) {
         Car myCar = new Car();
         System.out.println(myCar.toString());
+
+        String myCoffee = Coffee.COLOMBIAN.getToast();
+        
+        switch(myCoffee){
+            case "low" ->
+                System.out.println("Is low");
+            case "medium" ->
+                System.out.println("Is medium");
+        }
     }
 }
