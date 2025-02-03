8c495bdfc8be7de36fa3941764282ffd13bb8858
AngelKode <200angel@live.com.mx>
2025-02-02 23:02:50 -0600

    New project. Start stream learning

---
+++ streams/src/main/java/org/angelkode/basics/Basics.java
@@ -0,0 +1,36 @@
+package org.angelkode.basics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class Basics {
+    public static void main(String[] args) {
+        //Creating basic stream variable
+        Stream<Integer> sizes = Stream.of(1,2,3,4,5,25,40,-30-1);
+        sizes.forEach(System.out::println);
+
+        //Creating from array object
+        Double[] currentGrades = {5.0,6.0,5.0,3.0,9.0,10.0};
+        Stream<Double> gradesStream = Arrays.stream(currentGrades);
+        gradesStream.forEach(System.out::println);
+
+        //Using Stream builder
+        Stream<String> emails = Stream.<String>builder()
+                .add("email@ejemplo.com")
+                .build();
+        emails.forEach(System.out::println);
+
+        //From Collection class
+        List<Integer> myList = new ArrayList<>();
+        myList.add(2);
+        myList.add(3);
+
+        myList.stream().forEach(System.out::println);
+
+
+
+    }
+}
