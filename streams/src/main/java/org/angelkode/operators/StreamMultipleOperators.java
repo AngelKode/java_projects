5f2542fc48b9d560f133ef69ac812a380cb48981
AngelKode <200angel@live.com.mx>
2025-02-10 22:42:31 -0600

    Add: Streams with references and statistics

--- streams/src/main/java/org/angelkode/operators/StreamMultipleOperators.java
+++ streams/src/main/java/org/angelkode/operators/StreamMultipleOperators.java
@@ -1,9 +1,10 @@
 package org.angelkode.operators;
 
-import org.angelkode.models.User;
-
-import java.util.List;
+import java.util.IntSummaryStatistics;
 import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamMultipleOperators {
@@ -29,6 +30,24 @@ public class StreamMultipleOperators {
 
         totalGrade.ifPresentOrElse(System.out::println, () -> System.out.println("No grade present"));
 
+        //Working with double and int stream
+        DoubleStream gradeRange = DoubleStream.of(0,1,2,3,4,5);
 
+        double squareSum = gradeRange.reduce(0,(acc, value) -> {
+            return acc + Math.pow(value,2);
+        });
+
+        System.out.println(squareSum);
+        //
+        IntStream numberSum = IntStream.rangeClosed(0,10);
+        OptionalInt maxNum = numberSum.max();
+        maxNum.ifPresent(System.out::println);
+
+        //Statistics
+        IntStream randomNumbers = IntStream.rangeClosed(0,10);
+        IntSummaryStatistics summaryStatistics = randomNumbers.summaryStatistics();
+
+        System.out.println(summaryStatistics.getAverage());
+        System.out.println(summaryStatistics.getMin());
     }
 }
