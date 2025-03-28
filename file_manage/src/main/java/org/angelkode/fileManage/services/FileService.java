e3f544fe9c5b2fae924e5313fbd85770918d3a30
AngelKode <200angel@live.com.mx>
2025-03-27 23:35:44 -0600

    Use BufferedWriter example

--- file_manage/src/main/java/org/angelkode/fileManage/services/FileService.java
+++ file_manage/src/main/java/org/angelkode/fileManage/services/FileService.java
@@ -2,6 +2,7 @@ package org.angelkode.fileManage.services;
 
 import org.angelkode.fileManage.interfaces.FileBaseService;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,15 +14,10 @@ public class FileService extends FileBaseService {
         //Create File
         File fileToCreate = new File(fileName);
 
-        try {
-            //Create object to write into file
-            FileWriter fw = new FileWriter(fileToCreate, true);
-
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileToCreate, true))){
+            //For big writing data, we can use BufferedWriter, its more efficient than FileWriter
             //Set content
-            fw.append("Creating first content").append(System.lineSeparator());
-
-            //Close data stream
-            fw.close();
+            bufferedWriter.append("Creating first content\n");
         }catch (IOException e){
             System.out.println(e.getLocalizedMessage());
         }
