2ad744922d5296f8e610f39b62786e86ec9ab756
AngelKode <200angel@live.com.mx>
2025-08-12 00:07:33 -0600

    Add: System properties

--- introduction/basics/src/main/java/com/learning/Environment.java
+++ introduction/basics/src/main/java/com/learning/Environment.java
@@ -1,15 +1,34 @@
 package com.learning;
 
+import java.io.FileInputStream;
 import java.util.Map;
+import java.util.Properties;
 
 public class Environment {
     public static void main(String[] args) {
-        
-        Map<String,String> myEnv = System.getenv();
-        
-        System.out.println("Environment values: ");
-        for(String key : myEnv.keySet()){
-            System.out.println(key + " => " + myEnv.get(key));
+
+        //Get file of properties
+        try(FileInputStream propertyFileStream = new FileInputStream("./basics/src/config.properties")) {
+            System.out.println("Reading file...");
+
+            //Load custom prop file
+            Properties customProperties = new Properties(System.getProperties());
+            customProperties.load(propertyFileStream);
+
+            //Edit prop object
+            customProperties.setProperty("server.maxRequests","10");
+
+            //Load new properties
+            System.setProperties(customProperties);
+        }catch (Exception e) {
+            System.out.println("Error while reading file: \n" + e.getMessage());
         }
+
+        System.getProperties().list(System.out);
+
+
+        //--------------------------ENV---------------------------------
+
+        System.out.println(System.getenv());
     }
 }
