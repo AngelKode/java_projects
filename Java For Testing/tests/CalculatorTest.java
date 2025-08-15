32b01d063964dcb01a4c83001631c55639e306f5
AngelKode <200angel@live.com.mx>
2025-08-15 00:12:46 -0600

    Add: Parameters and Optional decorator

--- Java For Testing/tests/CalculatorTest.java
+++ Java For Testing/tests/CalculatorTest.java
@@ -1,9 +1,34 @@
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 @Test()
 public class CalculatorTest {
+
+    @BeforeClass
+    public void setUpTestEnv() throws InterruptedException {
+        System.out.println("Setting up test environment...");
+        Thread.sleep(2000);
+        System.out.println("Setting up test environment finished.");
+    }
+
+    @BeforeMethod
+    public void setUpTestMethod() {
+        System.out.println("Setting up test method...");
+    }
+
+    @AfterMethod
+    public void closingTestMethod() {
+        System.out.println("Closing test method...");
+    }
+
+    @AfterClass
+    public void tearDownTestEnv() throws InterruptedException {
+        System.out.println("Tearing down test environment...");
+        System.out.println("Closing selenium object...");
+        Thread.sleep(2000);
+        System.out.println("Test env finished successfully!");
+    }
+
     @DataProvider
     public Object[] testArrayData(){
         return new Object[][]{
@@ -12,17 +37,21 @@ public class CalculatorTest {
     }
 
     @Test(
-            testName = "Same length array",
-            description = "This test validates if the expected array has the same length than the others",
-            singleThreaded = true,
+            testName = "TC-01 Happy Path",
+            description = "TC to validate happy path",
             priority = 1,
             dataProvider = "testArrayData")
     public void testEqualArraySize(Integer[] data){
         Assert.assertEquals(new Integer[]{1,2,3,48}.length,data.length);
     }
 
-    @Test(dependsOnMethods = {"testEqualArraySize"})
-    public void testSameDataArray(){
+    @Test(
+            dependsOnMethods = {"testEqualArraySize"},
+            description = "TC for regression",
+            testName = "TC-02 Regression Test")
+    @Parameters({"dataExample"})
+    public void testSameDataArray(@Optional("Default data") String dataExample){
+        System.out.println(dataExample);
         System.out.println("testEqualArraySize completed successfully!");
         Assert.assertEquals(true,true);
     }
