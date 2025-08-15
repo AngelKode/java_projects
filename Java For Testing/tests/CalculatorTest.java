b3e5724a2e35c42f7481eaaefc0a6086b65a2191
AngelKode <200angel@live.com.mx>
2025-08-14 23:47:40 -0600

    Add: Testing parameters, dependency of methods and priority

--- Java For Testing/tests/CalculatorTest.java
+++ Java For Testing/tests/CalculatorTest.java
@@ -7,12 +7,23 @@ public class CalculatorTest {
     @DataProvider
     public Object[] testArrayData(){
         return new Object[][]{
-            new Integer[]{1,2,3,48}, new Integer[]{1,2,3},
+            new Integer[]{1,2,3,48}, new Integer[]{1,2,3,4},
         };
     }
 
-    @Test(dataProvider = "testArrayData")
+    @Test(
+            testName = "Same length array",
+            description = "This test validates if the expected array has the same length than the others",
+            singleThreaded = true,
+            priority = 1,
+            dataProvider = "testArrayData")
     public void testEqualArraySize(Integer[] data){
         Assert.assertEquals(new Integer[]{1,2,3,48}.length,data.length);
+    }
+
+    @Test(dependsOnMethods = {"testEqualArraySize"})
+    public void testSameDataArray(){
+        System.out.println("testEqualArraySize completed successfully!");
+        Assert.assertEquals(true,true);
     }
 }
