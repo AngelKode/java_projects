1ffaafb0f08e8a0e357d1802dd62476b6e8afa48
AngelKode <200angel@live.com.mx>
2025-09-26 18:04:41 -0600

    Add: Lazy data-provider using iterators to run tests

--- testng-basics/test/UI/BasicTestDataProvider.java
+++ testng-basics/test/UI/BasicTestDataProvider.java
@@ -2,6 +2,10 @@ package UI;
 
 import org.testng.annotations.DataProvider;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class BasicTestDataProvider {
     @DataProvider(
         name = "dbDataProvider"
@@ -11,5 +15,30 @@ public class BasicTestDataProvider {
                 {"jdbc",12345,8080},
                 {"mysql",54321,8081}
         };
+    }
+
+    @DataProvider(
+        name = "login-users-data"
+    )
+    public static Iterator<String> dataLoginUsers() {
+        //Create the object of the items and its own iterator
+        List<String> userProfiles = List.of("user-john","user-maria","user-pepe");
+
+        return userProfiles.iterator();
+    }
+
+    @DataProvider(
+        name = "login-users-all"
+    )
+    public static Iterator<Object[]> dataLoginUsersAll() {
+        //Create the object of the items and its own iterator
+        Object[][] usersProfileData = {
+            {25, "MEX", true, "ONLINE"},
+            {21, "USA", true, "DISCONNECTED"},
+            {45, "MEX", false,"BUSY"},
+            {19, "ENG", false,"OFFLINE"}
+        };
+
+        return Arrays.stream(usersProfileData).iterator();
     }
 }
--- testng-basics/test/UI/BasicTestNG.java
+++ testng-basics/test/UI/BasicTestNG.java
@@ -14,4 +14,27 @@ public class BasicTestNG {
         System.out.println("With password " + dbPassword);
         System.out.println("With port " + dbPort);
     }
+
+    //Lazy Data provider test
+    @Test(
+        dataProvider = "login-users-data",
+        dataProviderClass = BasicTestDataProvider.class,
+        priority = 1
+    )
+    public void loginTest(String username) {
+        System.out.println("Running test for user: " + username);
+    }
+
+    //Lazy Data provider with multiple params
+    @Test(
+            dataProvider = "login-users-all",
+            dataProviderClass = BasicTestDataProvider.class,
+            priority = 2
+    )
+    public void loginTest(int age, String country, boolean isMember, String status) {
+        System.out.println("Running test with user with age: " + age);
+        System.out.println("Country: " + country);
+        System.out.println("Is member: " + isMember);
+        System.out.println("Status: " + status);
+    }
 }
