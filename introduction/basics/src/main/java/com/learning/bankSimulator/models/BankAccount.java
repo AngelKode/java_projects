85e401be90c0fba1fc09ff7643a5130b27aa7907
AngelKode <200angel@live.com.mx>
2025-10-07 22:10:49 -0600

    Add: account operation withdraw and deposit

--- introduction/basics/src/main/java/com/learning/bankSimulator/models/BankAccount.java
+++ introduction/basics/src/main/java/com/learning/bankSimulator/models/BankAccount.java
@@ -9,21 +9,31 @@ public class BankAccount implements AccountOperation {
 
     private double balance;
     private long accountNumber;
-    private ReentrantLock lock;
 
     public BankAccount(double initialBalance) {
         this.balance = initialBalance;
         this.accountNumber = 0;
-        this.lock = new ReentrantLock();
     }
 
     @Override
-    public void withdraw(double amount) {
-        System.out.println("Withdrawing " + amount + " from bank account " + accountNumber);
+    public synchronized void withdraw(double amount) {
+        if(this.balance < amount) {
+            System.out.println("Insufficient funds. Operation rejected :(");
+            return;
+        }
+        System.out.print("Withdrawing " + amount + " from bank account " + accountNumber);
+        this.balance -= amount;
+        System.out.println(". New balance: " + this.balance);
     }
 
     @Override
-    public void deposit(double amount) {
-        System.out.println("Depositing " + amount + " from bank account " + accountNumber);
+    public synchronized void deposit(double amount) {
+        if(amount < 0.0D){
+            System.out.println("Unable to deposit negative values. Operation rejected :(");
+            return;
+        }
+        System.out.println("Depositing " + amount + " to bank account " + accountNumber);
+        this.balance += amount;
+        System.out.println(". New balance: " + this.balance);
     }
 }
