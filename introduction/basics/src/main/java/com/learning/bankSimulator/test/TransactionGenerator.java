a1a52322d48605218bd4dedabfd86c252a9a4b55
AngelKode <200angel@live.com.mx>
2025-10-07 22:16:41 -0600

    Add: executorservice to execute threads

--- introduction/basics/src/main/java/com/learning/bankSimulator/test/TransactionGenerator.java
+++ introduction/basics/src/main/java/com/learning/bankSimulator/test/TransactionGenerator.java
@@ -2,14 +2,19 @@ package com.learning.bankSimulator.test;
 
 import com.learning.bankSimulator.models.Bank;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @lombok.Setter
 @lombok.NoArgsConstructor
 public class TransactionGenerator {
 
     private Bank bank;
+    ExecutorService executor;
 
     public TransactionGenerator(Bank bank) {
         this.bank = bank;
+        this.executor = Executors.newFixedThreadPool(10);
     }
 
     public void generateTransactions(int numberOfTransactions) {
