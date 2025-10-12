2f5ce789ae29f5d458abd202c639c9c961dcdeef
AngelKode <200angel@live.com.mx>
2025-10-11 23:13:00 -0600

    Add: Bank transfer between 2 accounts implementation

--- introduction/basics/src/main/java/com/learning/bankSimulator/services/TransactionService.java
+++ introduction/basics/src/main/java/com/learning/bankSimulator/services/TransactionService.java
@@ -6,7 +6,40 @@ import com.learning.bankSimulator.models.BankAccount;
 public class TransactionService{
 
     public static void transferMoney(BankAccount originAccount, BankAccount destAccount, double amount) {
-        //TODO
+
+        System.out.println("---------------------------------------------------------------------------------------");
+        if(originAccount == null || destAccount == null){
+            System.out.println("Unable to transfer money because the bank account doesnt exist.");
+            System.out.println("---------------------------------------------------------------------------------------");
+            return;
+        }
+
+        System.out.println("Initiating transfer...");
+        System.out.println("Validating account money available...");
+
+        if(originAccount.getBalance() <= 0 || originAccount.getBalance() < amount){
+            System.out.print("Unable to transfer money due to insufficient balance.");
+            System.out.print("Reason: Amount to transfer: " + amount + " | Money available:" + originAccount);
+            System.out.println("---------------------------------------------------------------------------------------");
+            return;
+        }
+
+        System.out.print("Transferring " + amount + " to " + destAccount);
+
+        try{
+            //Subtract the amount to transfer from the origin account to the destination account
+            originAccount.setBalance(originAccount.getBalance() - amount);
+
+            //Deposit the amount to the destination account
+            destAccount.deposit(amount);
+
+            System.out.print("Successfully operation.\nNew Balance: Origin-" + originAccount + "New Balance: Dest-" + destAccount);
+        }catch (InterruptedException ex){
+            System.out.println("Error while transfer execution. " + ex.getMessage());
+        }catch (Exception ex){
+            System.out.println("Unable to execute transfer. " + ex.getMessage());
+        }
+        System.out.println("---------------------------------------------------------------------------------------");
     }
 
     public static void accountOperation(BankAccount originAccount, double amount, TransactionType type) throws InterruptedException {
@@ -16,14 +49,14 @@ public class TransactionService{
             return;
         }
 
-        System.out.println("Starting transaction to " + originAccount.getAccountNumber() + "...");
+        System.out.print("Starting transaction to " + originAccount);
         if(type == TransactionType.DEPOSIT) {
             originAccount.deposit(amount);
         }else{
             originAccount.withdraw(amount);
         }
 
-        System.out.println("Transaction completed");
+        System.out.print("Transaction completed\nNew Balance: " + originAccount);
     }
 
 }
