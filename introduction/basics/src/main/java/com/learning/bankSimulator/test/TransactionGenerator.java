08549f87a5825f28ec497e62534aa8ae7ad033e7
AngelKode <200angel@live.com.mx>
2025-11-27 23:43:45 -0600

    Add: implement random operation generator use. TODO implementation of every type of transaction

--- introduction/basics/src/main/java/com/learning/bankSimulator/test/TransactionGenerator.java
+++ introduction/basics/src/main/java/com/learning/bankSimulator/test/TransactionGenerator.java
@@ -1,6 +1,8 @@
 package com.learning.bankSimulator.test;
 
+import com.learning.bankSimulator.enums.TransactionType;
 import com.learning.bankSimulator.models.Bank;
+import com.learning.bankSimulator.services.RandomOperationGenerator;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,9 +20,24 @@ public class TransactionGenerator {
     }
 
     public void generateTransactions(int numberOfTransactions) {
-        //TODO
+        //To get a transaction type between DEPOSIT, WITHDRAW or TRANSFER
+        RandomOperationGenerator randomOperationGenerator = new RandomOperationGenerator();
+        TransactionType transactionTypeSelection;
         for (int i = 0; i < numberOfTransactions; i++) {
+            //For every transaction cycle, get the actual transaction type to be performed
+            transactionTypeSelection = randomOperationGenerator.getNewRandomTransactionType();
 
+            if(transactionTypeSelection == TransactionType.DEPOSIT) {
+                //TODO
+            }
+
+            if(transactionTypeSelection == TransactionType.WITHDRAW) {
+                //TODO
+            }
+
+            if(transactionTypeSelection == TransactionType.TRANSFER) {
+                //TODO
+            }
         }
     }
 
